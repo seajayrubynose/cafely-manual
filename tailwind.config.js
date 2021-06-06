4ee5f537ecc2bcd938c8a55b9ae7c5b2d48fae90
Blankeos <carloantonioct@gmail.com>
2021-06-06 19:52:48 +0800

    Some variants and theme extensions

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,26 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#e89700", // our primary orange
+        brown: {
+          100: "#c09A6D",
+          200: "#573F27",
+          300: "#3b2d26", // my fave brown
+        },
+        darkGray: "#282f42",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      scale: ["active", "group-hover"],
+      backgroundColor: ["active"],
+      textColor: ["active"],
+      opacity: ["disabled"],
+      outline: ["active"],
+    },
   },
   plugins: [],
 };
