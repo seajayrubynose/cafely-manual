c55b084f2565f4033cb9ef41cc3a8bb1bfcbd89f
Blankeos <carloantonioct@gmail.com>
2021-06-12 18:39:02 +0800

    Utility func for fetching data from content folder

--- lib/data.tsx
+++ lib/data.tsx
@@ -1,10 +1,25 @@
 import fs from "fs";
 import path from "path";
+import matter from "gray-matter";
 
 const contentDirectory = path.join(process.cwd(), "content");
+const manualDirectory = path.join(contentDirectory, "manual");
 
-export function getAllContent() {
-  const allContent = fs.readdirSync(contentDirectory);
+export function getAllManualContent() {
+  const allManualContent = fs.readdirSync(manualDirectory);
 
-  console.log(allContent);
+  return allManualContent.map((fileName) => {
+    const slug = fileName.replace(".md", "");
+    const fileContents = fs.readFileSync(
+      path.join(manualDirectory, fileName),
+      "utf8"
+    );
+    const { data, content } = matter(fileContents);
+
+    return {
+      data,
+      content,
+      slug,
+    };
+  });
 }
