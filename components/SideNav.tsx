3c757e719acf2c9011ffb3d67fe7d72aa53d784b
Blankeos <carloantonioct@gmail.com>
2021-06-12 18:38:41 +0800

    Wrote some props for SideNav

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,18 +1,32 @@
+import Link from "next/link";
 import React from "react";
 
-const SideNav: React.FC = () => {
+interface SideNavProps {
+  links: [{ slug: string; title: string }];
+}
+
+const SideNav = ({ links }: SideNavProps) => {
   return (
     <div className="bg-primary p-5 pt-16 flex-grow w-64">
       <ul className="text-sm flex flex-col space-y-4 text-white">
-        <li className="hover:text-yellow-300">Getting Started</li>
-        <li className="hover:text-yellow-300">Logging In To Your Account</li>
+        {links &&
+          links.map((link) => {
+            return (
+              <Link href={`/manual/${link.slug}`}>
+                <a>
+                  <li className="hover:text-yellow-300">{link.title}</li>
+                </a>
+              </Link>
+            );
+          })}
+        {/* <li className="hover:text-yellow-300">Logging In To Your Account</li>
         <li className="hover:text-yellow-300">Change Account Info</li>
         <li className="hover:text-yellow-300">Signing Out</li>
         <li className="hover:text-yellow-300">Viewing Other User's Profiles</li>
         <li className="hover:text-yellow-300">Sharing Your Profile</li>
         <li className="hover:text-yellow-300">Submitting A Review</li>
         <li className="hover:text-yellow-300">Searching for Products</li>
-        <li className="hover:text-yellow-300">Adding A Product</li>
+        <li className="hover:text-yellow-300">Adding A Product</li> */}
       </ul>
     </div>
   );
