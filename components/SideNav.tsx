6a03228cc8f0411cf016dff63691e5cec5d2c35f
Blankeos <carloantonioct@gmail.com>
2021-06-22 14:55:59 +0800

    overflow-y-auto in SideNav

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -9,7 +9,7 @@ interface SideNavProps {
 const SideNav = ({ additionalClass, links }: SideNavProps) => {
   return (
     <div
-      className={`overflow-y-scroll bg-primary p-5 pt-16 flex-grow flex-shrink-0 w-56" ${additionalClass}`}
+      className={`overflow-y-auto bg-primary p-5 pt-16 flex-grow flex-shrink-0 w-56" ${additionalClass}`}
       style={{
         boxShadow: "7px 0px 20px 0px rgba(50, 50, 50, 0.20);",
       }}
