0e82b7b6182575967158dd59134de67772a7bf80
Blankeos <carloantonioct@gmail.com>
2021-06-24 01:27:00 +0800

    Added animations for SideNav (not responsive).

--- components/SideNav.tsx
+++ components/SideNav.tsx
@@ -1,17 +1,43 @@
 import Link from "next/link";
-import React from "react";
+import React, { useEffect } from "react";
+import { motion, useAnimation } from "framer-motion";
 
 interface SideNavProps {
   additionalClass?: string;
   links: [{ slug: string; title: string }];
+  isActive: boolean;
 }
 
-const SideNav = ({ additionalClass, links }: SideNavProps) => {
+const SideNav = ({ additionalClass, links, isActive }: SideNavProps) => {
+  const animation = useAnimation();
+
+  useEffect(() => {
+    if (isActive) {
+      animation.start({
+        x: 0,
+        transition: {
+          ease: "circOut",
+        },
+      });
+    } else {
+      animation.start({
+        x: "-100vw",
+        transition: {
+          ease: "circIn",
+        },
+      });
+    }
+  }, [isActive]);
+
   return (
-    <div
-      className={`overflow-y-auto bg-primary p-5 pt-16 flex-grow flex-shrink-0 w-56" ${additionalClass}`}
+    <motion.div
+      animate={animation}
+      className={`overflow-y-auto bg-primary p-5 pt-16 flex-grow flex-shrink-0 w-56" absolute z-10 h-full md:block md:relative md:h-auto ${
+        ""
+        // isActive && "hidden"
+      }`}
       style={{
-        boxShadow: "7px 0px 20px 0px rgba(50, 50, 50, 0.20);",
+        boxShadow: "7px 0px 20px 0px rgba(50, 50, 50, 0.20)",
       }}
     >
       <ul className="text-sm flex flex-col space-y-4 text-white">
@@ -26,7 +52,7 @@ const SideNav = ({ additionalClass, links }: SideNavProps) => {
             );
           })}
       </ul>
-    </div>
+    </motion.div>
   );
 };
 
